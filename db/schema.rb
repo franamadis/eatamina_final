e38e80594c88f831ed81186ba90dda1046a111ec
David Martins <davidvicentemartins@gmail.com>
2018-06-20 12:41:25 +0200

    Added Effect column to additives table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_19_124957) do
+ActiveRecord::Schema.define(version: 2018_06_20_103706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_06_19_124957) do
     t.string "risk"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "effect"
   end
 
   create_table "product_additives", force: :cascade do |t|
