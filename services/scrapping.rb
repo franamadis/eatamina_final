9db76492bec9abbae31a57aa03510c84b083f6d3
David Martins <davidvicentemartins@gmail.com>
2018-06-20 18:07:05 +0200

    json file corrected - no more wrong cases -, DB populated, seed file populated

--- services/scrapping.rb
+++ services/scrapping.rb
@@ -4,50 +4,51 @@ require 'nokogiri'
 require 'pry'
 require 'json'
 
-def secondpage(chemical)
-  pages = "http://www.aditivos-alimentarios.com/2016/01/" + chemical + ".html"
-  html_file = open(pages).read
-  html_doc = Nokogiri::HTML(html_file)
+class Scraper
+  def secondpage(chemical)
+    pages = "http://www.aditivos-alimentarios.com/2016/01/" + chemical + ".html"
+    html_file = open(pages).read
+    html_doc = Nokogiri::HTML(html_file)
 
-  blocks = html_doc.search('blockquote')
-    description = blocks[0].text.strip
-    effect = blocks[5].text.strip
+    blocks = html_doc.search('blockquote')
+      description = blocks[0].text.strip
+      if ["Baja", "Media", "Alta"].include?(blocks[5])
+        effect = blocks[4].text.strip
+      else
+        effect = blocks[5].text.strip
+      end
 
-  detail_hash = {
-    description: description,
-    effect: effect
-  }
-  return detail_hash
-
-# Daves code not hashes of arrays
-  # secondpagecontents = { secondpagecontents: [] }
-  # descripts = html_doc.search('.post-body blockquote').text
-end
-
-
-def scrapping
-  puts "working...."
-  puts "😯"
-  url = "http://www.aditivos-alimentarios.com/p/lista.html"
-  html_file = open(url).read
-  html_doc = Nokogiri::HTML(html_file)
-  enumbers = { enumbers: []}
-  rows = html_doc.search('tbody tr')
-
-  rows.each do |r|
-
-    chemical = r.children[1].text.strip
-    p chemical
-    name = r.children[3].text.strip
-    risk = r.children[5].text.strip
-    description = secondpage(chemical)
-    enumbers[:enumbers] << {chemical: chemical, name: name, risk: risk, description: description[:description], effect: description[:effect]}
-  # binding.pry
+    detail_hash = {
+      description: description,
+      effect: effect
+    }
+    return detail_hash
   end
 
-  File.open('enumbers.json', 'wb') do |file|
-    file.write(JSON.pretty_generate(enumbers))
+  def scrapping
+    puts "working...."
+    puts "😯"
+    url = "http://www.aditivos-alimentarios.com/p/lista.html"
+    html_file = open(url).read
+    html_doc = Nokogiri::HTML(html_file)
+    enumbers = { enumbers: []}
+    rows = html_doc.search('tbody tr')
+
+    rows.each do |r|
+
+      chemical = r.children[1].text.strip
+      p chemical
+      name = r.children[3].text.strip
+      risk = r.children[5].text.strip
+      description = secondpage(chemical)
+      enumbers[:enumbers] << {chemical: chemical, name: name, risk: risk, description: description[:description], effect: description[:effect]}
+
+      a = Additive.create!(chemical: chemical, name: name, risk: risk, description: description[:description], effect: description[:effect])
+
+    end
+
+    File.open('enumbers.json', 'wb') do |file|
+      file.write(JSON.pretty_generate(enumbers))
+    end
   end
 end
-
-scrapping
