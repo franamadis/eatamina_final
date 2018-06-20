eebf71260e8617ec6b4e072772a97c03278490d2
David Martins <davidvicentemartins@gmail.com>
2018-06-20 13:17:52 +0200

    changed code to chemical in additives table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_103706) do
+ActiveRecord::Schema.define(version: 2018_06_20_110250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "additives", force: :cascade do |t|
     t.string "name"
-    t.string "code"
+    t.string "chemical"
     t.string "description"
     t.string "risk"
     t.datetime "created_at", null: false
