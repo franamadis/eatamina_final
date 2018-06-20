f58510a0d6b97ed9d5081a6728b456788ec9f659
Henrique Silva <hbs003@hotmail.com>
2018-06-20 14:06:26 +0200

    mvc3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,14 +14,14 @@ class PagesController < ApplicationController
       params[:query]}.json"
       response_serialized = open(url).read
       response = JSON.parse(response_serialized).flatten
+      response2 = response.find {|item| item.class == Hash}
 
       if response.include?("product found")
-        new_product = Product.create!(sku: params[:query], status: "accepted", name: response[1]['product_name'], photo: response[1]['image_url'])
-        # raise
-        redirect_to product_path(new_product)
+        @new_product = Product.create!(sku: params[:query], status: "accepted", name: response2['product_name'], photo: response2['image_url'])
+        redirect_to product_path(@new_product)
       else
-        new_product = Product.create(sku:params[:query])
-        redirect_to new_product_path(new_product)
+        @new_product = Product.create(sku:params[:query])
+        redirect_to new_product_path(@new_product)
       end
     end
   end
