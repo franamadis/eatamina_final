f352a64577548607e2b3b2ee5f53d21605586915
Henrique Silva <hbs003@hotmail.com>
2018-06-20 12:45:50 +0200

    mvc234

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,13 +8,16 @@ class PagesController < ApplicationController
 
   def home
     if params[:query].present?
-      @product = {}
-      @product[:sku] = params[:query]
-      url = "https://world.openfoodfacts.org/api/v0/product/#{@product[:sku]}.json"
+      # @product = {}
+      # @product[:sku] = params[:query]
+      url = "https://world.openfoodfacts.org/api/v0/product/#{
+      params[:query]}.json"
       response_serialized = open(url).read
       response = JSON.parse(response_serialized).flatten
-      if response['status_verbose'] == "product found"
-        new_product = Product.create(sku: params[:query], status: "accepeted", name: response['product_name'], photo:response['image_url']  )
+
+      if response.include?("product found")
+        new_product = Product.create!(sku: params[:query], status: "accepted", name: response[1]['product_name'], photo: response[1]['image_url'])
+        # raise
         redirect_to product_path(new_product)
       else
         new_product = Product.create(sku:params[:query])
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,9 @@
 class Product < ApplicationRecord
+
   has_many :additives, through: :product_additives
   has_many :users, through: :queries
-  validates :name, uniqueness: true, presence: true
-  validates :sku, uniqueness: true, presence: true
-  mount_uploader :photo, PhotoUploader
-  # def requests
-  #   products.map { |product| product.status == "pending" }.flatten
-  # end
 
-  # Product.requests
-  # scope :requests, -> {where(status: 'pending')}
-  #before the best way to right the method, means the same
+
   def self.requests
     Product.where(status: 'pending')
   end
