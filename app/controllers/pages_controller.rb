a5c4506bea5595d759cc129db69f836844be0747
Fran Amadis <info@franamadis.com>
2018-06-20 14:49:01 +0200

    fixed photo when view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,7 @@ class PagesController < ApplicationController
 
   def home
     if params[:query].present?
+      puts "AEFAEGSRGAGARGARG"
       # @product = {}
       # @product[:sku] = params[:query]
       url = "https://world.openfoodfacts.org/api/v0/product/#{
@@ -17,11 +18,15 @@ class PagesController < ApplicationController
       response2 = response.find {|item| item.class == Hash}
 
       if response.include?("product found")
+        puts "AEFAEGS1111111111111111111RGARG"
         @new_product = Product.create!(sku: params[:query], status: "accepted", name: response2['product_name'], photo: response2['image_url'])
+        p @new_product
         redirect_to product_path(@new_product)
       else
-        @new_product = Product.create(sku:params[:query])
-        redirect_to new_product_path(@new_product)
+        puts "AEFAEGSRGA222222222222222222222GARGARG"
+        @new_product = Product.create!(sku:params[:query])
+
+        render new_product_path
       end
     end
   end
