ca35aa6d6525e0935b8e0fd1bbf82d741e6cebfc
Fran Amadis <info@franamadis.com>
2018-06-25 13:57:24 +0200

    rollback

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'dashboard/requests'
-  get 'additive/show'
+  get "products/message", to: "products#message"
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products, only: [:index, :new, :create, :show, :edit, :update]
 
   resources :additives, only: [:show]
 
