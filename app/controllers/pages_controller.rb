41e39e10d14556ccabb5ce70125a8ea2aeefadcc
Henrique Silva <hbs003@hotmail.com>
2018-06-27 12:03:54 +0200

    ssssss

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,9 @@ require 'json'
 require 'open-uri'
 
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :howwork]
+  skip_before_action :authenticate_user!, only: [:home, :howwork, :method]
   after_action :set_query, only: :home
+  after_action :set_product_id_in_session, only: :home
 
   def howwork
   end
@@ -15,7 +16,7 @@ class PagesController < ApplicationController
       response_serialized = open(url).read
       response = JSON.parse(response_serialized).flatten
       response2 = response.find {|item| item.class == Hash}
-      if response2 != nil
+      if response2 != nil && response2["labels"] != nil
          organic = response2["labels"].include? "organic"
       else
          organic = false
@@ -35,12 +36,11 @@ class PagesController < ApplicationController
         if check_product?
           @new_product = Product.where(sku: params[:query]).first
         else
-          @new_product = Product.create!(sku: params[:query], status: "pending", name: response2['product_name'], photo: response2['image_url'], nutritional_info: response2['nutriments'], prod_add: response2['additives_tags'], brand: response2['brands'], nutrition_grade: response2['nutrition_grades'], organic: organic, )
+          @new_product = Product.create!(sku: params[:query], status: "pending", name: response2['product_name'], photo: response2['image_url'], nutritional_info: response2['nutriments'], prod_add: response2['additives_tags'], brand: response2['brands'], nutrition_grade: response2['nutrition_grades'], organic: organic )
         end
         redirect_to products_message_path
       else
         @new_product = Product.create!(sku: params[:query], status: "pending", response: "product found")
-        session[:new_product] = @new_product
         redirect_to products_message_path
       end
     end
@@ -55,6 +55,10 @@ class PagesController < ApplicationController
     else
       return false
     end
+  end
+
+  def set_product_id_in_session
+    session[:new_product_id] = @new_product.id
   end
 
   def set_query
